594fef431c86c0222f2b9dd1ec1bf71c8e99fb4a
JanaP30 <JanaP30@users.noreply.github.com>
2022-04-18 20:35:01 +0200

    first commit

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatMessage;
+use App\Models\ChatRoom;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function room(Request $request)
+    {
+        return ChatRoom::all()->paginate(5);
+    }
+
+    public function messages(Request $request, $roomId)
+    {
+        return ChatMessage::where('chat_room_id', $roomId)
+        ->with('user')
+        ->orderBy('created_at', 'DESC')
+        ->get();
+    }
+
+    public function newMessage(Request $request, $roomId)
+    {
+        $newMessage = new ChatMessage;
+        $newMessage->user_id = Auth::id();
+        $newMessage->chat_room_id = $roomId;
+        $newMessage->message = $request->message;
+        $newMessage->save();
+        return $newMessage;
+    }
+
+
+
+
+
+    
+}
